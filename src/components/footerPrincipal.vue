6d1da0d027d1dfb07bf32a7e98e9b21a354ad77f
Carlos Arturo Moreno <39220043+mrcamssys@users.noreply.github.com>
2025-03-11 05:57:24 +0000

    feat: se crea el head y footer

---
+++ src/components/footerPrincipal.vue
@@ -0,0 +1,61 @@
+<template>
+    <footer class="text-center bg-body-tertiary">
+        <!-- Grid container -->
+        <div class="container pt-4">
+            <!-- Section: Social media -->
+            <section class="mb-4">
+                <a v-for="(social, index) in socialLinks" :key="index"
+                    class="btn btn-link btn-floating btn-lg text-body m-1" :href="social.link" role="button"
+                    data-mdb-ripple-color="dark">
+                    <i :class="social.icon"></i>
+                </a>
+            </section>
+            <!-- Section: Social media -->
+        </div>
+        <!-- Grid container -->
+
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05); color:aliceblue">
+            © {{ currentYear }} Copyright:
+            <a class="text-body" href="https://atomomshop.com/">Atomomshop SAS</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            socialLinks: [
+                { icon: "fab fa-facebook-f", link: "#!" },
+                { icon: "fab fa-twitter", link: "#!" },
+                { icon: "fab fa-google", link: "#!" },
+                { icon: "fab fa-instagram", link: "#!" },
+                { icon: "fab fa-linkedin", link: "#!" },
+                { icon: "fab fa-github", link: "#!" }
+            ],
+            currentYear: new Date().getFullYear()
+        };
+    }
+};
+</script>
+
+<style scoped>
+footer {
+    background-color: var(--footer-color);
+    color: var(--text-color);
+    padding: 20px 0;
+}
+
+.btn-link {
+    color: var(--text-color) !important;
+}
+
+.btn-link:hover {
+    color: var(--accent-color) !important;
+}
+.text-body{
+    color: var(--accent-color) !important;
+}
+</style>
---
+++ src/components/menuPortada.vue
@@ -0,0 +1,77 @@
+<template>
+<div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-custom">
+        <div class="container">
+            <div class="logo-container">
+                <a class="navbar-brand" href="#">
+                    <img src="/imagenes/logo2.png" alt="Logo" class="logo" />
+                </a>
+            </div>
+            
+            <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="navbar-toggler-icon"></span>
+</button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li v-for="(item, index) in menuItems" :key="index" class="nav-item">
+                        <a class="nav-link" :class="{ active: item.active }" :href="item.link">
+                            <i :class="item.icon"></i> {{ item.name }}
+                        </a>
+                    </li>
+                    <!--<li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown link
+                            </a>
+                           <!- - <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>-- >
+                        </li>-->
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            menuItems: [{
+
+                    name: "Portada",
+                    link: "#",
+                    active: true,
+                    icon: "bi bi-house"
+                },
+                {
+                    name: "Contacto",
+                    link: "#",
+                    icon: "bi bi-envelope"
+                }
+            ]
+        };
+    }
+}
+</script>
+
+<style scoped>
+.logo {
+  width: 130px;
+  /*margin-bottom: 1rem;*/
+}
+.logo-container {
+  top: 0rem;
+  position: absolute;
+  padding: 0.3rem;
+  padding-bottom: 1rem;
+  background-color: var(--navbar-color-logo) !important;
+  clip-path: polygon(0% 0%, 100% 0%, 100% 80%, 1% 100%);;
+  padding: 15px 30px;
+  text-align: center;
+}
+
+</style>
